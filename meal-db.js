a51ee560216c832987fa41545acb6c71a0ba77da
rasel-3 <rasel.mm217@gmail.com>
2023-07-20 21:38:22 +0600

    add meals detail

---
+++ meal-db.js
@@ -0,0 +1,69 @@
+const loadMeals = (search) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
+    fetch(url)
+        .then(response => response.json())
+        .then(data => displayMeals(data.meals))
+}
+
+const displayMeals = (meals) => {
+    const mealsContainer = document.getElementById('meals-container');
+    mealsContainer.innerHTML = '';
+    meals.forEach(meal => {
+        // console.log(meal)
+        const mealDiv = document.createElement('div');
+        mealDiv.classList.add('col');
+        mealDiv.innerHTML = `
+  <div onclick="loadMealDetail(${meal.idMeal})" class="card">
+  <img src="${meal.strMealThumb
+            }" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${meal.strMeal
+            }</h5>
+    <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+  </div>
+</div>`;
+
+        mealsContainer.appendChild(mealDiv);
+
+    });
+}
+
+
+const searchFood = () => {
+    const searchField = document.getElementById('search-field')
+    const searchText = searchField.value;
+    loadMeals(searchText);
+    searchField.value = '';
+}
+
+const loadMealDetail= (idmeal) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idmeal}`
+    fetch(url)
+        .then(response => response.json())
+        .then(data => displayMealDetail(data.meals[0]));
+
+}
+
+const displayMealDetail = (meal) => {
+    const detailContainer = document.getElementById('detail-container');
+    detailContainer.innerHTML='';
+    const mealDiv = document.createElement('div');
+    mealDiv.classList.add('card')
+    mealDiv.innerHTML = `
+<img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+<div class="card-body">
+  <h5 class="card-title">${meal.strMeal}</h5>
+  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  <a href="#" class="btn btn-primary">Go somewhere</a>
+</div>
+`;
+detailContainer.appendChild(mealDiv)
+}
+
+
+
+
+
+
+
+
